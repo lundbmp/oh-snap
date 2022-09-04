e7150c12d17dfad29f303911d6ea45415aa50fa2
lundbmp <m.lundberg008@gmail.com>
2022-09-04 14:12:40 -0500

    added tests for the nav bar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -22,7 +22,7 @@ function Nav() {
   return (
     <header>
       <h2>
-        <a href="/">
+        <a data-testid="link" href="/">
           <span role="img" aria-label="camera">
             {" "}
             📸
@@ -33,14 +33,18 @@ function Nav() {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about">About me</a>
+            <a data-testid="about" href="#about">
+              About me
+            </a>
           </li>
           <li>
             <span>Contact</span>
           </li>
           {categories.map((category) => (
             <li className="mx-1" key={category.name}>
-              <span onClick={() => categorySelected(category.name)}>{category.name}</span>
+              <span onClick={() => categorySelected(category.name)}>
+                {category.name}
+              </span>
             </li>
           ))}
         </ul>
